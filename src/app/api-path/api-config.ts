68e529d9631cb8be84874f8ab3a09551c3e00788
aman9893 <aman9893jain@gmail.com>
2022-10-01 15:31:42 +0530

    add

---
+++ src/app/api-path/api-config.ts
@@ -0,0 +1,29 @@
+export const apiConfig = {
+
+    localhostUrl:"http://ec2-43-206-128-164.ap-northeast-1.compute.amazonaws.com:3011/",
+
+    registerData: 'api/register',
+    loginData: 'api/login',
+    updateUser:'api/update_user',
+    getAdminProfile: 'api/get_admin_profile',
+
+    getTable: 'api/get_table',
+    updateTable: 'api/update_table',
+    addTable:'api/add_table',
+    delete_table:'api/delete_table',
+
+    getMenu: 'api/get_menu',
+    updateMenu: 'api/update_menu',
+    addMenu:'api/add_menu',
+    deleteMenu:'api/delete_menu',
+
+    getBill: 'api/bill_list',
+    updateBill: 'api/update_bill_info',
+    addBill:'api/addbill_data',
+    deleteBill:'api/delete_bill',
+    getbillByID:'api/getbill_byTableid'
+
+
+/////////////////////////////////////////////////////////////////////////////////
+
+}
---
+++ src/app/service/data.service.ts
@@ -0,0 +1,88 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from  '@angular/common/http';
+import { apiConfig } from '../api-path/api-config';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http :HttpClient,private snackBar: MatSnackBar,) { 
+  }
+  
+  registerData(data:any) {
+    return this.http.post(apiConfig.localhostUrl + apiConfig.registerData, data);
+  }
+  loginData(data:any) {
+    return this.http.post(apiConfig.localhostUrl + apiConfig.loginData, data);
+  }
+  updateData(data:any) {
+    return this.http.put(apiConfig.localhostUrl + apiConfig.updateUser, data);
+  }
+  getAdminProfileDataById(id:any){
+    return this.http.get(apiConfig.localhostUrl+apiConfig.getAdminProfile + '/' + id);
+   }
+  ///////////////////////////table ////////////////////////////////////////
+  updateTable(data:any){
+    return this.http.put(apiConfig.localhostUrl + apiConfig.updateTable, data);
+  }
+  deleteTable(id:any){
+    return this.http.delete(apiConfig.localhostUrl + apiConfig.delete_table+'/'+id);
+  }
+  saveTable(data:any){
+    return this.http.post(apiConfig.localhostUrl + apiConfig.addTable, data);
+  }
+  getTableInfo() {
+    return this.http.get(apiConfig.localhostUrl + apiConfig.getTable);
+  }
+//////////////////////////////menu ///////////////////////////////////
+
+
+  updateMenu(data:any){
+    return this.http.put(apiConfig.localhostUrl + apiConfig.updateMenu, data);
+  }
+  deleteMenu(id:any){
+    return this.http.delete(apiConfig.localhostUrl + apiConfig.deleteMenu+'/'+id);
+  }
+  saveMenu(data:any){
+    return this.http.post(apiConfig.localhostUrl + apiConfig.addMenu, data);
+  }
+  getMenuInfo() {
+    return this.http.get(apiConfig.localhostUrl + apiConfig.getMenu);
+  }
+
+
+//////////////////////////////////////////////////////////////////////////////////
+
+updateBill(data:any){
+  return this.http.put(apiConfig.localhostUrl + apiConfig.updateBill, data);
+}
+deleteBill(id:any){
+  return this.http.delete(apiConfig.localhostUrl + apiConfig.deleteBill+'/'+id);
+}
+saveBill(data:any){
+  return this.http.post(apiConfig.localhostUrl + apiConfig.addBill, data);
+}
+getBillInfo() {
+  return this.http.get(apiConfig.localhostUrl + apiConfig.getBill);
+}
+
+compelteOrder(id:any){
+  return this.http.delete(apiConfig.localhostUrl + apiConfig.deleteBill+'/'+id);
+}
+
+getBillByTableID(id:any){
+  return this.http.get(apiConfig.localhostUrl + apiConfig.getbillByID+'/'+id);
+  
+}
+
+
+openSnackBar(message: string, action: string) {
+  this.snackBar.open(message, action, {
+    duration: 3000,
+    panelClass: ['app-bottom-snackbar'],
+  });
+}
+
+}
