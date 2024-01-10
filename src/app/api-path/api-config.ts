6df1cbce5e89c04b86b019f24dd72a79820a02ae
aman9893 <aman9893jain@gmail.com>
2024-01-11 00:58:12 +0530

    add

--- src/app/api-path/api-config.ts
+++ src/app/api-path/api-config.ts
@@ -1,7 +1,7 @@
 export const apiConfig = {
 
-   // localhostUrl:"https://restrohelp.com/",
-     localhostUrl:"http://localhost:8000/",
+    localhostUrl:"https://18.217.110.106:3011/",
+   //  localhostUrl:"http://localhost:8000/",
 
 
     registerData: 'api/register',
